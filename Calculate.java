4cfb5c87448fb649fd0a88d10f00eb23e3618362
egor90wd <egor90wd@gmail.com>
2015-06-21 00:14:31 +0300

    Calculate creating

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,8 @@
 public class Calculate {
 	public static void main(String[] args){
-		System.out.println("Calculate...");
+		int f = Integer.valueOf(args[0]);
+		int s = Integer.valueOf(args[1]);
+		int summ = f + s;
+		System.out.println("Summ " + summ);
 	}
 }
